9331696e0cc09ab0d27f7d386eb64638e5807f2b
IT21314742 <it21314742@my.sliit.lk>
2024-03-14 11:00:25 +0530

    Update JSX file
    add js

--- Login-Signup/src/components/LoginSignup/loginSignup.jsx
+++ Login-Signup/src/components/LoginSignup/loginSignup.jsx
@@ -10,6 +10,7 @@ export const loginSignup = () => {
     <div className="container">
         <div className="header">
             <div className="text">Sign Up</div>
+            <div className="underline"></div>
         </div>
     </div>
   )
