999b339cc186de8e6827ea64d35248547513a494
IT21314742 <it21314742@my.sliit.lk>
2024-03-14 10:57:43 +0530

    add png
    add png directory to JSX file

--- Login-Signup/src/components/LoginSignup/loginSignup.jsx
+++ Login-Signup/src/components/LoginSignup/loginSignup.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './LoginSignup.css'
 
-import user_icon from '../Assets/'
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
 
 export const loginSignup = () => {
   return (
